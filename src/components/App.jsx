671d59b04ed91da3747b58ef84088279c26a5f94
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-06-15 14:47:33 +1200

    Capture input into state variable and use it to update list item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState(""); //variable to capture input typed by user
+  const [addedItem, setAddedItem] = useState(""); //variable to display list item
+
+  function handleChange(event) {
+    const newItem = event.target.value;
+    setInput(newItem); //updates the input variable
+  }
+
+  function addItem() {
+    setAddedItem(input); //pushes the input to the list
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={input} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          <li>{addedItem}</li>
         </ul>
       </div>
     </div>
